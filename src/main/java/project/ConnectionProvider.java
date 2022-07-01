c8ad61f8873dd5373d2aa2e4adf68755f6f3157d
stitch <stitch@localhost>
2022-07-01 12:27:03 -0700

    Hotel Management System Java Application

---
+++ src/main/java/project/ConnectionProvider.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project;
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ *
+ * @author stitch
+ */
+public class ConnectionProvider {
+    public static Connection getCon(){
+       try{
+           Class.forName("com.mysql.cj.jdbc.Driver");
+           Connection con = DriverManager.getConnection("jdbc:mysql://localhost/hotel","root","68706870");
+           return con;
+       }
+       catch(Exception e){
+           return null;
+       }
+    }
+}
